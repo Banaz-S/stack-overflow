8606de2845e45c5f79434d7b5b40bf4b532b1e6f
Aso A. Sdiq <aso.ascs2021@uoh.edu.iq>
2025-10-19 22:34:44 +0300

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@ import DefaultLayout from "@/components/layouts/DefaultLayout.vue";
 </script>
 
 <template>
-  <Component :is="$route.meta.layout ? $route.meta.layout : DefaultLayout" />
+  <Component :is="$route?.meta.layout ? $route.meta.layout : DefaultLayout" />
 </template>
